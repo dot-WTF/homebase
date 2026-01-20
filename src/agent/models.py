6246bacfb98ede0ba5e2251f30936da3c5790458
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-19 23:14:25 -0500

    chore(agent): add openai model names

---
+++ src/agent/models.py
@@ -0,0 +1,8 @@
+from enum import StrEnum 
+
+class OpenAIModels(StrEnum):
+    GPT_4O_MINI = "gpt-4o-mini"
+    GPT_4O = "gpt-4o"
+    GPT_o3_MINI = "o3-mini"
+    GPT_o4_MINI = "o4-mini"
+    GPT_4_1 = "gpt-4.1-2025-04-14"
