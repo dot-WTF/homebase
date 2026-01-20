ae334bad8707ff8fa1d7774e696f7032a763ddb8
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-19 20:47:15 -0500

    Initialize uv project :)

---
+++ setup.py
@@ -0,0 +1,90 @@
+import os 
+import sys
+import platform
+import subprocess
+from enum import StrEnum
+
+# We use UNIX here to refer to any *NIX system including Linux and macOS
+class MachineSystem(StrEnum):
+    UNIX = "unix" 
+    WINDOWS = "windows"
+
+class PythonExecutable(StrEnum):
+    PIP_WINDOWS = "pip"
+    PIP_UNIX = "pip3"
+    PYTHON_WINDOWS = "python"
+    PYTHON_UNIX = "python3"
+
+def setup_uv(system: MachineSystem) -> bool:
+    if system == MachineSystem.WINDOWS:
+        try: 
+            subprocess.run(["curl", "-LsSf", "https://astral.sh/uv/install.sh", "|", "sh"], shell=True, check=True)
+        except Exception as e:
+            print(f"ERROR INSTALLING UV:\n {e}")
+            return False
+    else:
+        try:
+            subprocess.run(["powershell", "-ExecutionPolicy", "ByPass", "-c", "irm https://astral.sh/uv/install.ps1 | iex"], shell=True, check=True)
+        except Exception as e:
+            print(f"ERROR INSTALLING UV:\n {e}")
+            return False
+    return True
+
+def initialize_project()-> bool:
+    try:
+        subprocess.run(["uv", "init" "."], check=True) 
+    except Exception as e:
+        print(f"ERROR INITIALIZING PROJECT:\n {e}")
+        return False
+    return True
+
+def setup_venv()-> bool:
+    try:
+        subprocess.run(["uv", "venv"], check=True) 
+    except Exception as e:
+        print(f"ERROR SETTING UP VENV:\n {e}")
+        return False
+    return True
+
+def install_dependencies(pip_executable: PythonExecutable): 
+    try:
+        subprocess.run([pip_executable, "install", "-r", "requirements.txt"], check=True)
+    except Exception as e:
+        print(f"ERROR INSTALLING DEPENDENCIES:\n {e}")
+        return False
+    return True
+
+def activate_venv(system: MachineSystem):    
+    activate_script = system == MachineSystem.WINDOWS and ".\\.venv\\Scripts\\activate" or "source .venv/bin/activate"
+    try:
+        subprocess.run(activate_script, shell=True, check=True)
+    except Exception as e:
+        print(f"ERROR ACTIVATING VENV:\n {e}")
+        return False
+    return True
+
+def main():
+    system = platform.system().lower() == MachineSystem.WINDOWS and MachineSystem.WINDOWS or MachineSystem.UNIX
+
+    python_executable = system == MachineSystem.WINDOWS and PythonExecutable.PYTHON_WINDOWS or PythonExecutable.PYTHON_UNIX
+    
+    setup_uv_result = setup_uv(system)
+    initialize_project_result = initialize_project()
+    setup_venv_result = setup_venv()
+    install_dependencies_result = install_dependencies(python_executable)
+    activate_venv_result = activate_venv(system)
+
+    result_base = [
+        setup_uv_result,
+        initialize_project_result,
+        setup_venv_result,
+        install_dependencies_result,
+        activate_venv_result
+    ]
+
+    if not all(result_base):
+        print("One or more setup steps failed. Please check the error messages above.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
