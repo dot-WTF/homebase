e43213010cce89f331d0ccedc3ec5a1ac537dab6
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-19 23:12:26 -0500

    chore(agent): set up template tools for implementation

---
+++ src/agent/tools.py
@@ -0,0 +1,64 @@
+from langchain.tools import tool 
+
+from src.utils.net import send_request
+from src.agent.schema import (
+    ToggleAlarmSchema, ToggleLightSchema, ToggleDoorSchema,
+    ToggleDoorLockSchema, ToggleFanSchema, 
+    SetLightSchema, SetDoorOpenSchema, SetDoorLockSchema,
+    SetThermostatSchema, SetAlarmSchema, SetBlindsSchema, SetFanSchema
+)
+from src.agent.prompts import HouseActionsPrompts
+
+# Lights control tools
+@tool
+def toggle_light():
+    send_request() 
+
+@tool
+def set_light():
+    send_request() 
+
+# Door control tools 
+@tool
+def toggle_door():
+    send_request()
+
+@tool
+def set_door_open():
+    send_request()
+
+@tool
+def toggle_door_lock():
+    send_request()
+
+@tool
+def set_door_lock():
+    send_request()
+
+# Blinds control tools 
+@tool
+def set_blinds():
+    send_request()
+
+# Thermostat control tools
+@tool
+def set_thermostat():
+    send_request()
+
+# Fans control tools 
+@tool
+def toggle_fan():
+    send_request()
+
+@tool
+def set_fan():  
+    send_request()
+
+# Alarm control tools
+@tool
+def toggle_alarm():
+    send_request()
+
+@tool
+def set_alarm():    
+    send_request()
