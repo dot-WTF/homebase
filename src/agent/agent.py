d0b55379cceee4d9c14fce9352c6fa69c01cc903
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-21 22:41:13 -0500

    fix(agent): fix parameter type bugs

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -8,14 +8,14 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 assert OPENAI_API_KEY is not None, "OPENAI_API_KEY environment variable is not set."
 
 
-def create_model(model_name: OpenAIModels, temperature: float = 0) -> ChatOpenAI:
+def create_model(model_name: str, temperature: float = 0) -> ChatOpenAI:
     return ChatOpenAI(
-        model_name=model_name.value,
+        model_name=model_name,
         temperature=temperature,
         openai_api_key=OPENAI_API_KEY  
     )
 
-def create_house_agent(model_name: OpenAIModels, temperature: float = 0, verbose: bool = True) -> any:
+def create_house_agent(model_name: str, temperature: float = 0, verbose: bool = True) -> any:
     llm = create_model(model_name, temperature=temperature)
     agent = create_agent(
         llm=llm,
