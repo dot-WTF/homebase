96913aaf8a2a5b418467070e2ecd71d66b86b517
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-22 00:42:32 -0500

    feat(.): implement main agent orcherstration loop

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
-def main():
-    print("Hello from wth-agent!")
+import keyboard
+from src.agent.agent import create_house_agent
+from src.utils.audio import listen_for_audio, test_device_availabaility
+from src.utils.stt import transcribe_audio
 
+def main():
+    # Test audio device availability 
+    is_audio_available = test_device_availabaility()
+    assert is_audio_available, "No audio input device available."
+
+    # Define agent with default configurations
+    print("Creating house agent...")
+    agent = create_house_agent()
+    print("House agent created. Press 'Enter' to give a command or 'Esc' to exit.")
+
+    while True:
+        if keyboard.is_pressed('esc'):
+            print("Exiting program.")
+            break
+
+        if keyboard.is_pressed('enter'):
+            try:  
+                user_command_audio = listen_for_audio() 
+                command_transcription = transcribe_audio(user_command_audio)
+                if command_transcription is not None:
+                    result = agent.invoke(command_transcription)
+                    print(f"Agent Response: {result}")
+                else:
+                    print("Failed to transcribe audio.")
+
+            except Exception as e:
+                # Gracefully handle errors during agent invocation
+                print(f"Error during agent invocation: {e}")
 
 if __name__ == "__main__":
     main()
