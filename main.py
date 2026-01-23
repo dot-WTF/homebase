28a59ed87864e36d51670026069a7d15e3a049e9
Vinlaw Mudehwe <vmudehwe3661@gmail.com>
2026-01-22 23:46:59 -0500

    feat(.): add keyboard interaction and fix agent invocation bug

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import keyboard
 import sys
+from langchain.messages import HumanMessage
 from src.agent.agent import create_house_agent
 from src.utils.audio import listen_for_audio, test_device_availabaility
 from src.utils.stt import transcribe_audio
+from enum import StrEnum
+
+class InputCommands(StrEnum):
+    INTERACT = "i"
+    QUIT = "q"
 
 def main():
     # Test audio device availability 
@@ -15,16 +20,24 @@ def main():
     print("House agent created. Press 'Enter' to give a command or 'Esc' to exit.")
 
     while True:
-        if keyboard.is_pressed('esc'):
+        # MacOS is strict with detecting keyboard interactions so we use simple input prompts
+        user_input = input("Enter command (i for interact, q for quit): ")
+        if user_input == InputCommands.QUIT:
             print("Exiting program.")
             sys.exit(0)
 
-        if keyboard.is_pressed('enter'):
+        if user_input == InputCommands.INTERACT:
             try:  
+                print("Listening for command...")
                 user_command_audio = listen_for_audio() 
+                print("Audio captured. Transcribing...")
                 command_transcription = transcribe_audio(user_command_audio)
+                print(f"Transcription: {command_transcription}")
                 if command_transcription is not None:
-                    result = agent.invoke(command_transcription)
+                    print("Audio transcribed. Invoking agent...")
+                    result = agent.invoke(
+                        {"messages": [{"role": "user", "content": command_transcription}]}
+                    )
                     print(f"Agent Response: {result}")
                 else:
                     print("Failed to transcribe audio.")
